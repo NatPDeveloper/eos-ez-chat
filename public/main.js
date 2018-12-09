3fcc6a26599adc06b934a426261b84837bbf22af
natpdeveloper <natanaelprudhomme@DESKTOP-HV680VC.localdomain>
2018-12-09 17:46:22 -0500

    testing scatter

--- public/main.js
+++ public/main.js
@@ -7,6 +7,61 @@ $(function() {
     '#3b88eb', '#3824aa', '#a700ff', '#d300e7'
   ];
 
+  const network = {
+    blockchain:'eos',
+    protocol:'http',
+    host:'jungle.eosn.io',
+    port:80,
+    chainId:'e70aaab8997e1dfce58fbfac80cbbb8fecec7b99cf982a9444273cbc64c41473'
+  }
+
+  ScatterJS.scatter.connect("eosezchatnat").then(connected => {
+    // User does not have Scatter Desktop, Mobile or Classic installed.
+    if(!connected) return false;
+
+    const scatter = ScatterJS.scatter;
+
+    // Now we need to get an identity from the user.
+    // We're also going to require an account that is connected to the network we're using.
+    const requiredFields = { accounts:[network] };
+
+    scatter.getIdentity(requiredFields).then(() => {
+
+        // Always use the accounts you got back from Scatter. Never hardcode them even if you are prompting
+        // the user for their account name beforehand. They could still give you a different account.
+        const account = scatter.identity.accounts.find(x => x.blockchain === 'eos');
+
+        // You can pass in any additional options you want into the eosjs reference.
+        const eosOptions = { expireInSeconds:60 };
+
+        // Get a proxy reference to eosjs which you can use to sign transactions with a user's Scatter.
+        var eos = scatter.eos( network, Eos, eosOptions )
+
+        // ----------------------------
+        // Now that we have an identity,
+        // an EOSIO account, and a reference
+        // to an eosjs object we can send a transaction.
+        // ----------------------------
+
+
+        // Never assume the account's permission/authority. Always take it from the returned account.
+        const transactionOptions = { authorization:[`${account.name}@${account.authority}`] };
+
+        eos.sendmsg(account.name, 'eosezchatnat', transactionOptions).then(trx => {
+            // That's it!
+            console.log(`Transaction ID: ${trx.transaction_id}`);
+        }).catch(error => {
+            console.error(error);
+        });
+
+    }).catch(error => {
+        // The user rejected this request, or doesn't have the appropriate requirements.
+        console.error(error);
+    });
+
+    window.ScatterJS = null;
+  });
+
   // Initialize variables
   var $window = $(window);
   var $usernameInput = $('.usernameInput'); // Input for username
@@ -18,13 +73,6 @@ $(function() {
 
   // Prompt for setting a username
   var username;
-
-  /***************** */
-  let status = "(status) ";
-  let pending = "(pending) ";
-  let reversible = "(reversible) ";
-  let irreversible = "(irreversible) ";
-  /***************** */
 
   var connected = false;
   var typing = false;
