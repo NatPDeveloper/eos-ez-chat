7a0dd3008c26d944b4e64337c9a35340338a0af5
natpdeveloper <natanaelprudhomme@DESKTOP-HV680VC.localdomain>
2018-12-11 23:11:05 -0500

    change API endpoint to https

--- public/main.js
+++ public/main.js
@@ -13,9 +13,9 @@ $(function() {
 
   const network = {
     blockchain:'eos',
-    protocol:'http',
-    host:'jungle.eosn.io',
-    port:80,
+    protocol:'https',
+    host:'jungle2.cryptolions.io',
+    port:443,
     chainId:'e70aaab8997e1dfce58fbfac80cbbb8fecec7b99cf982a9444273cbc64c41473'
   }
 
